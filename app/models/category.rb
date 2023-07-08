ab0565b14d506eb4460a6cfc4c9e9dd985654ced
monicah79 <monniewanjema@gmail.com>
2023-07-09 00:45:25 +0300

    Fix linters

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,8 +3,6 @@ class Category < ApplicationRecord
   has_one_attached :icon
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-
-
   validates :name, presence: true
   validates :icon, presence: true
 end
